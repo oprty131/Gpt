055f77a895f171cbd36b354740cef2289685db55
oprty131 <slwmplylw@gmail.com>
2024-12-22 17:31:29 +0200

    Update and rename bot.py to index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const { Client, GatewayIntentBits, ChannelType, PermissionsBitField } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+
+app.listen(port, () => {
+  console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
+});
+
+client.once('ready', () => {
+  console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[34mPing: ${client.ws.ping} ms \x1b[0m`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return; // Ignore bot messages
+
+  if (message.content.startsWith('*say')) {
+    try {
+      // Extract the message after the *say command
+      const sayMessage = message.content.slice(5).trim(); // Get the part after *say
+
+      if (!sayMessage) {
+        return message.channel.send('You need to specify a message to say!');
+      }
+
+      // Delete the user's message
+      await message.delete();
+
+      // Send the message that the user requested
+      message.channel.send(sayMessage);
+    } catch (error) {
+      console.error('Error during *say operation:', error);
+    }
+  }
+});
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log('\x1b[36m[ LOGIN ]\x1b[0m', `\x1b[32mLogged in as: ${client.user.tag} ✅\x1b[0m`);
+    console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[35mBot ID: ${client.user.id} \x1b[0m`);
+    console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[34mConnected to ${client.guilds.cache.size} server(s) \x1b[0m`);
+  } catch (error) {
+    console.error('\x1b[31m[ ERROR ]\x1b[0m', 'Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+login();
